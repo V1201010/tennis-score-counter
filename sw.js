036eeb0d53d4c80d4708c3faaab7103370d366af
Vladislav <ten.meta@gmail.com>
2026-06-14 21:57:38 +0300

    Pull-to-refresh on setup screen clears saved match and stays on setup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tennis-score-v9';
+const CACHE_NAME = 'tennis-score-v10';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
