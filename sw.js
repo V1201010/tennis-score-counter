eecb3bd71f037a78e592160a9cd9b2b9c0eeb334
Vladislav <ten.meta@gmail.com>
2026-06-14 20:35:36 +0300

    Fix vertical scrolling on landscape orientation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tennis-score-v7';
+const CACHE_NAME = 'tennis-score-v8';
 const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
